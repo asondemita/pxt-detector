9df0f792ad65f881ddea79901c94e319f40bc329
asondemita <cyacya@moto-macbook.local>
2018-06-11 16:56:48 +0900

    test2

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\u30A1" color=#EC7505 block="test"
+namespace test {
+
+    //% blockId=show_strings block="hogehoge %v"
+    export function putStr(text: string): void {
+      basic.showLeds(`
+          . . . . .
+          . # . # .
+          . . . . .
+          # . . . #
+          . # # # .
+          `);
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
+“test.ts”
 // tests go here; this will not be compiled when this package is used as a library
+basic.forever(() => {
+  test.putStr("カタカナ")
+})
+
